fc39182dd018b904c84e3a77b8e6818c5d14ac30
fabiotosi92 <fabio.tosi5@unibo.it>
2019-09-11 14:16:20 +0200

    added image

---
+++ test.sh
@@ -0,0 +1,7 @@
+#!bin/bash
+
+image_path=./images/000000_10.png
+output_path=./output
+
+#check inference
+python main.py --test_single --image_path $image_path --output_path $output_path --save_images 
