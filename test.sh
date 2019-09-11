66cf7da216daba6d75c3e692355c6a684af56f89
fabiotosi92 <fabio.tosi5@unibo.it>
2019-09-11 15:44:20 +0200

    update pier

--- test.sh
+++ test.sh
@@ -4,4 +4,4 @@ image_path=./images/000000_10.png
 output_path=./output
 
 #check inference
-python main.py --test_single --image_path $image_path --output_path $output_path --save_images 
+python main.py --test_single --image_path $image_path --output_path $output_path --save_images
